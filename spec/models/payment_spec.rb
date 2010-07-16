f2a1f652f4ae87fa6b243f6c8b4d4b76a6bb7d25
Mathieu Davy <matt@ekynoxe.com>
2010-07-16 16:17:46 +0100

    adding payment description and tests

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Payment do
   before(:each) do
     @valid_attributes = {
-      :value=>23.45
+      :value=>2345,
+      :description=>"a payment"
     }
     @payment = Payment.new
   end
@@ -21,6 +22,11 @@ describe Payment do
   it "should not be valid with a non numerical value" do
     @payment.attributes = @valid_attributes.except(:value)
     @payment.value = "string value"
+    @payment.should_not be_valid
+  end
+  
+  it "should not be valid without a description" do
+    @payment.attributes = @valid_attributes.except(:description)
     @payment.should_not be_valid
   end
   
